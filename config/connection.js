18a5f4b9695280a1049e5bffb912216ae675fb41
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-03-22 13:54:08 -0500

    tested GET routes in insomnia

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
-// import the Sequelize constructor from the library
+// import the sequelize constructor from library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our db
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-// });
+// // set an editable variable for sequelize so it can be changed depending on the launch enviornment
+// let sequelize;
+
+// // heroku connection
+// if (process.env.JAWSDB_URL) {
+//     sequelize = new Sequelize(process.env.JAWSDB_URL);
+//   } else {
+//     // create connection to our database, pass in your MySQL info
+//     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         port: 3306
+//     });
+// }
 
 const sequelize = new Sequelize('tech_blog_db', 'root', 'Pokemon23?', {
   host: 'localhost',
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -10,31 +9,38 @@ const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
+    secret: '53cr3t50m3th1ng',
+    cookie: {
+        maxAge: 300 * 1000
+    },
     resave: false,
+
+    rolling: true,
     saveUninitialized: true,
     store: new SequelizeStore({
-      db: sequelize
+        db:sequelize
     })
 };
 
 app.use(session(sess));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(require('./controllers/'));
 
-// Set 'force:true' to re-create tables and association changes.
 sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
+
+// Set 'force:true' to re-create the tables and association changes.
