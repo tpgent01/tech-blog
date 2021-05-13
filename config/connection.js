a2f2106a4a4d2756e9ad91c1cf371162bc703680
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-05-12 21:35:27 -0500

    cant get connection to work

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
-const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-  });
+const Sequelize = require('sequelize');
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+const sequelize = new Sequelize('tech_blog_db', 'root', 'Pokemon23?', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
 
 module.exports = sequelize;
